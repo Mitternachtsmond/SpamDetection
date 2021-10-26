88cf2e1eacfc24572f0aa8d6afa92a531b3328c6
Satwik2002 <satwik2002.ss@gmail.com>
2021-10-27 00:57:50 +0530

    Final app

---
+++ app.py
@@ -0,0 +1,30 @@
+import re
+from flask import Flask,render_template,request
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer as tf
+import pickle
+import nltk
+
+
+
+app = Flask(__name__)
+loaded_model = pickle.load(open('SVM.sav', 'rb'))
+fin = pickle.load(open('vectorizer.pk', 'rb'))
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    if(request.method == 'GET'):
+        return render_template('index.html')
+    else:
+        text = request.form.get('text')
+        X = fin.transform([text])
+        boole = loaded_model.predict(X)[0]
+        if boole == 0:
+            result='not spam'
+        else:
+            result='spam'
+        return render_template ('result.html', text= text, result = result )
+
+
+
+if __name__ == 'main':
+    app.run(debug=True)
