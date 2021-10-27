57a5c762f39d9f4bd4d007c8801e50bbab8c4f7d
Gaurav Jain <gjain369@gmail.com>
2021-10-27 11:47:36 +0530

    error fix 1

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-import re
 from flask import Flask,render_template,request
-from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import TfidfVectorizer as tf
 import pickle
 import nltk
-
-
-
+nltk.download('punkt')
 app = Flask(__name__)
+
 loaded_model = pickle.load(open('SVM.sav', 'rb'))
 fin = pickle.load(open('vectorizer.pk', 'rb'))
+
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     if(request.method == 'GET'):
@@ -22,9 +19,7 @@ def index():
             result='not spam'
         else:
             result='spam'
-        return render_template ('result.html', text= text, result = result )
-
-
+        return render_template ('index.html', text= text, result = result )
 
 if __name__ == 'main':
-    app.run(debug=True)
+    app.run()
